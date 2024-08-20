cdfcbd8c8dfa6b12b886d78cb10fea94d1669ee5
samkujawa <122956587+samkujawa@users.noreply.github.com>
2024-08-19 23:01:32 -0400

    Add files via upload

---
+++ TeeBoxConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace GolfCourseApp
+{
+    public class TeeBoxConverter : Newtonsoft.Json.JsonConverter<List<Tee>>
+    {
+        public override void WriteJson(JsonWriter writer, List<Tee> value, JsonSerializer serializer)
+        {
+            // Implement serialization logic if needed
+            throw new NotImplementedException();
+        }
+
+        public override List<Tee> ReadJson(JsonReader reader, Type objectType, List<Tee> existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            JToken token = JToken.Load(reader);
+            var tees = new List<Tee>();
+
+            // Assuming each item in the JSON array is a Tee object
+            foreach (var teeToken in token)
+            {
+                var tee = teeToken.ToObject<Tee>();
+                if (tee != null)
+                {
+                    tees.Add(tee);
+                }
+            }
+
+            return tees;
+        }
+    }
+}
